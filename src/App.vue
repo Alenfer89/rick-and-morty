e624c5295cba1c2a6fe02e7bcccc767f0535ca4b
Alenfer89 <alberioalex@gmail.com>
2022-08-31 10:55:27 +0200

    loader-reminder

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <Jumbotron />
     <Card />
     <ModalCard />
@@ -26,7 +25,8 @@ export default {
     Card,
     ModalCard,
     Pagination,
-    Favourite
+    Favourite,
+    //Loader
   },
   data: function(){
         return{
@@ -61,10 +61,5 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
